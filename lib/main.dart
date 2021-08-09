c47b7409e2e613df0c0cf371dfd50fb4cc24ddf8
fariqfgi <fariq.fgi@gmail.com>
2021-08-09 20:58:44 +0700

    AxisAlignment Property

--- lib/main.dart
+++ lib/main.dart
@@ -23,24 +23,29 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           child: Container(
             margin: EdgeInsets.only(left: 23.0, top: 0, right: 23.0, bottom: 0),
-            padding: EdgeInsets.only(left: 0, top: 50.0, right: 0, bottom: 0),
-            child: Column(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                Image(
-                  image: AssetImage('assets/images/ic_payment.png'),
-                  height: 200,
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 20.0, bottom: 4.0),
-                  child: Text(
-                    'Rich Together',
-                    style: mainHeader,
-                  ),
-                ),
-                Text(
-                  'Save your money little bit and we \nwill help to be rich.',
-                  style: subHeader,
-                  textAlign: TextAlign.center,
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Image(
+                      image: AssetImage('assets/images/ic_payment.png'),
+                      height: 200,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0, bottom: 4.0),
+                      child: Text(
+                        'Rich Together',
+                        style: mainHeader,
+                      ),
+                    ),
+                    Text(
+                      'Save your money little bit and we \nwill help to be rich.',
+                      style: subHeader,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
               ],
             ),
