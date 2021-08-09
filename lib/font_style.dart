01175b51453d2b35c6c84210aa0566d402ec46db
fariqfgi <fariq.fgi@gmail.com>
2021-08-09 21:03:50 +0700

    Remove Cache & Custom Fonts

--- lib/font_style.dart
+++ lib/font_style.dart
@@ -4,10 +4,12 @@ TextStyle mainHeader = TextStyle(
   fontSize: 26,
   color: Colors.blueGrey,
   fontFamily: 'Poppins',
+  fontWeight: FontWeight.w700,
 );
 
 TextStyle subHeader = TextStyle(
   fontSize: 14,
   color: Colors.grey,
   fontFamily: 'Poppins',
+  fontWeight: FontWeight.w300,
 );
